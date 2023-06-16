a94415b773bea6feb2fa5295cf576ce749a01cb4
clementrozario <clementcla@outlook.com>
2023-06-16 10:41:31 +0530

    javscript exercise

---
+++ exerise2.js
@@ -0,0 +1,10 @@
+
+const listitems=document.getElementsByClassName("list-group-item");
+
+listitems[2].style.backgroundColor="green";
+
+
+for(let i=0;i<listitems.length;i++){
+    listitems[i].style.fontWeight="bold";
+}
+
