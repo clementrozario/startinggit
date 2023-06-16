518ff5f775f061fcd12964a763c6c2fcb61918e7
clementrozario <clementcla@outlook.com>
2023-06-16 11:16:46 +0530

    abord

--- exerise2.js
+++ exerise2.js
@@ -1,10 +1,13 @@
+let newline=document.createElement("li");
+newline.textContent="new item";
 
-const listitems=document.getElementsByClassName("list-group-item");
+let listitem=document.getElementById("items");
+listitem.appendChild(newline);
+
+const listitems=document.getElementsByTagName("li");
 
 listitems[2].style.backgroundColor="green";
 
 
-for(let i=0;i<listitems.length;i++){
-    listitems[i].style.fontWeight="bold";
-}
-
+for(let i=0;i<listitems.length;i++)
+listitems[i].style.fontWeight="bold";
