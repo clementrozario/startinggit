a58c4d0e2d731aa3e7b1fe69d9e7b9beedc9c13d
clementrozario <clementcla@outlook.com>
2023-06-20 14:35:55 +0530

    local storage in correct format(objects)

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
+
 function saveInput() {
   // Get the name and email input values.
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
 
-  // Store the values in localStorage.
-  localStorage.setItem("name", name);
-  localStorage.setItem("email", email);
+  // Create a user object.
+  const user = {
+    name: name,
+    email: email,
+  };
+
+  // Store the user object in localStorage.
+  localStorage.setItem("user", JSON.stringify(user));
 }
 
 // Bind the saveInput() function to the submit button.
 document.getElementById("sumbit").addEventListener("click", saveInput);
+
+
